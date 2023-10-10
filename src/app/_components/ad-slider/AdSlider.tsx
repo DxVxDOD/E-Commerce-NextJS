f1056f4330a0875005fb5a091f73419f9998b536
DxVxDOD <orbangdavid@yahoo.com>
2023-10-10 23:56:26 +0300

    ad-slider functionality started

--- src/app/_components/ad-slider/AdSlider.tsx
+++ src/app/_components/ad-slider/AdSlider.tsx
@@ -4,10 +4,29 @@ import {useEffect} from "react";
 
 const AdSlider = () => {
 
+    const update = (newActive: HTMLElement, elements) => {
+        const newActivePosition = newActive.dataset.pos!
+        const current = elements
+    }
+
   const test = [1,1,1,1,1,1,1,1,1,1,1,11,1,1,1,1]
 
   useEffect(() => {
+    const carouselList = document.getElementById('carousel-list')!
+    const carouselListItem = document.querySelectorAll('.carousel-list-item')!
+    const elements = Array.from(carouselListItem);
 
+    let intervalId: number;
+
+    carouselList.addEventListener('click', (e) => {
+        const newActive = e.target! as HTMLElement;
+        const isItem = newActive.closest('.carousel-list-item')
+
+        if(!isItem || newActive.classList.contains('carousel-list-item-active')) {
+            return
+        }
+
+    })
   }, [])
 
   return (
@@ -18,8 +37,7 @@ const AdSlider = () => {
               {test.map((n, i) => {
               return(
                   <li
-                      id={'carousel-list-item'}
-                    className={'flex items-end justify-center w-[65vw] max-w-[1000px] aspect-[1.77/1] rounded-2xl absolute bg-zinc-950 border border-zinc-400 transition-all shadow-black shadow-md hover:border-zinc-50 hover:shadow-lg'}
+                    className={'carousel-list-item flex items-end justify-center w-[65vw] max-w-[1000px] aspect-[1.77/1] rounded-2xl absolute bg-zinc-950 border border-zinc-400 transition-all shadow-black shadow-md hover:border-zinc-50 hover:shadow-lg'}
                     key={i}>test
                 </li>
               )
