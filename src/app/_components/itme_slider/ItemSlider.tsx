36b93c213fddaca77cadae5f6782a7b9daec33aa
DxVxDOD <orbangdavid@yahoo.com>
2023-10-29 19:12:28 +0200

    item slider reworked, add progress indicator

--- src/app/_components/itme_slider/ItemSlider.tsx
+++ src/app/_components/itme_slider/ItemSlider.tsx
@@ -10,7 +10,8 @@ const ItemSlider = ({
   children: ReactNode;
   title: string;
 }) => {
-  const onButtonPress = (button: HTMLElement) => {
+
+  const onButtonPressOld = (button: HTMLElement) => {
     const progressBar: HTMLElement = button
       .closest(".row")!
       .querySelector(".progress-bar")!;
@@ -59,37 +60,75 @@ const ItemSlider = ({
     }
   };
 
-  const onButtonPress2 = (button: HTMLElement) => {
-    const slider: HTMLElement = button.closest('.progress-container')!.querySelector('.slider')!
+  // needs work, progress indicator doesn't move correctly
+  const moveProgressIndicator = (button: HTMLButtonElement, slider: HTMLElement) => {
+    const sliderIndex = parseInt(getComputedStyle(slider).getPropertyValue('--slider-index'));
+    const progressBar = button.closest('.row')!.querySelector('.progress-bar') as HTMLDivElement;
+    const progressBarItemCount = progressBar.children.length;
     const closestTitle: HTMLElement = button
       .closest(".row")!
       .querySelector(".slider")!;
 
-    let width = slider.offsetWidth
+   if(button.dataset.button === 'next' && title === closestTitle.dataset.title) {
+     if (sliderIndex - 1 < 0) {
+       slider.style.setProperty(
+         "--slider-index",
+         `${progressBarItemCount - 1}`,
+       );
+       progressBar.children[sliderIndex].classList.remove("active");
+       progressBar.children[progressBarItemCount - 1].classList.add("active");
+     } else {
+       slider.style.setProperty("--slider-index", `${sliderIndex - 1}`);
+       progressBar.children[sliderIndex].classList.remove("active");
+       progressBar.children[sliderIndex - 1].classList.add("active");
+     }
+   }
+   if (button.dataset.button === 'previous' && title === closestTitle.dataset.title) {
+     if (sliderIndex + 1 >= progressBarItemCount) {
+       slider.style.setProperty("--slider-index", `${0}`);
+       progressBar.children[sliderIndex].classList.remove("active");
+       progressBar.children[0].classList.add("active");
+     } else {
+       slider.style.setProperty("--slider-index", `${sliderIndex + 1}`);
+       progressBar.children[sliderIndex].classList.remove("active");
+       progressBar.children[sliderIndex + 1].classList.add("active");
+     }
+   }
+  }
 
-    const next = document.querySelector('[data-button="next"]') as HTMLElement
-    const previous = document.querySelector('[data-button="previous"]') as HTMLElement
+  const onButtonPress = (container: HTMLElement) => {
+    const slider = container.querySelector('.slider') as HTMLElement;
+    const childrenContainer = slider.querySelector('.children-container') as HTMLElement;
+    const next = container.closest('.row')!.querySelector('[data-button="next"]') as HTMLButtonElement;
+    const previous = container.closest('.row')!.querySelector('[data-button="previous"]') as HTMLButtonElement;
 
-    if( button.dataset.button === "next" &&
-      title === closestTitle.dataset.title ) {
-      slider.scrollBy(width + 16, 0);
-      if(slider.scrollWidth !== 0) {
-        previous.setAttribute('display', 'flex');
+    const gap = parseInt(getComputedStyle(childrenContainer).getPropertyValue('gap'));
+    let width = container.offsetWidth;
+
+    next.addEventListener('click', e => {
+      slider.scrollBy(width + gap, 0);
+      if (slider.scrollWidth !== 0) {
+        previous.style.visibility = 'visible';
       }
-      if(slider.scrollWidth - width - 16 <= slider.scrollLeft + width) {
-        next.setAttribute('display', 'none');
+      if (childrenContainer.scrollWidth - width - gap <= slider.scrollLeft + width) {
+        next.style.visibility = 'hidden';
       }
-    }
-    if( button.dataset.button === "previous" &&
-      title === closestTitle.dataset.title ){
-      slider.scrollBy(-(width + 16), 0);
-      if(slider.scrollLeft - width - 16 <= 0) {
-        previous.setAttribute('display', 'none');
+      // moveProgressIndicator(next, slider);
+    })
+
+    previous.addEventListener('click', e => {
+      slider.scrollBy(-(width + gap), 0);
+      if (slider.scrollLeft - width - gap <= 0) {
+        previous.style.visibility = 'hidden';
       }
-      if(slider.scrollWidth - width - 16 <= slider.scrollLeft + width) {
-        next.setAttribute('display', 'flex');
+      if (childrenContainer.scrollWidth - width - gap <= slider.scrollLeft + width) {
+        next.style.visibility = 'visible'
       }
-    }
+      // moveProgressIndicator(previous, slider);
+    })
+
+
+    window.addEventListener('resize', e => (width = container.offsetWidth));
   }
 
   const throttle = (callBackFunc: () => void, delay = 1000) => {
@@ -117,8 +156,6 @@ const ItemSlider = ({
     );
     const progressBarItemCount = Math.ceil(itemCount / itemPerScreen);
 
-    slider.style.setProperty('--items-per-screen', `${Math.ceil(itemCount / 3)}`);
-
     if (sliderIndex >= progressBarItemCount) {
       slider.style.setProperty("--slider-index", `${progressBarItemCount - 1}`);
       sliderIndex = progressBarItemCount - 1;
@@ -135,63 +172,20 @@ const ItemSlider = ({
   };
 
   useEffect(() => {
-    window.addEventListener(
-      "resize",
-      throttle(() => {
-        const progressBar = document.querySelectorAll(".progress-bar")!;
-        progressBar.forEach(calculateProgressBar);
-      }, 250),
-    );
 
-    const progressBar = document.querySelectorAll(".progress-bar")!;
-    progressBar.forEach(calculateProgressBar);
+    const progressContainers: NodeListOf<HTMLElement> = document.querySelectorAll('.slider-container');
+    progressContainers.forEach(onButtonPress);
 
-    document.addEventListener("click", (e) => {
-      let button: HTMLElement;
-      const eTarget = e.target as HTMLElement;
-      if (eTarget.matches(".button")) {
-        button = eTarget;
-      } else {
-        button = eTarget.closest(".button")!;
-      }
-      if (button !== null) onButtonPress(button);
-      // if (button !== null) onButtonPress2(button);
-    });
-  }, []);
-
-  useEffect(() => {
-
-    const progressContainer: NodeListOf<HTMLElement> = document.querySelectorAll('.progress-container');
-
-    progressContainer.forEach(container => {
-      let width = container.offsetWidth;
-
-      const slider = container.querySelector('.slider') as HTMLElement;
-      const next = container.querySelector('[data-button="next"]') as HTMLButtonElement;
-      const previous = container.querySelector('[data-button="previous"]') as HTMLButtonElement;
-
-      next.addEventListener('click', e => {
-        container.scrollBy(width + 16, 0);
-        if (container.scrollWidth !== 0) {
-          previous.setAttribute('display', 'flex');
-        }
-        if (slider.scrollWidth - width - 16 <= container.scrollLeft + width) {
-          next.setAttribute('display', 'none');
-        }
-      })
-
-      previous.addEventListener('click', e => {
-        container.scrollBy(-(width + 16), 0);
-        if (container.scrollLeft - width - 16 <= 0) {
-          previous.setAttribute('display', 'none')
-        }
-        if (!(slider.scrollWidth - width - 16 <= container.scrollLeft + width)) {
-          next.setAttribute('display', 'flex')
-        }
-      })
-
-      window.addEventListener('resize', e => (width = container.offsetWidth));
-    })
+    // window.addEventListener(
+    //   "resize",
+    //   throttle(() => {
+    //     const progressBar = document.querySelectorAll(".progress-bar")!;
+    //     progressBar.forEach(calculateProgressBar);
+    //   }, 250),
+    // );
+    //
+    // const progressBar = document.querySelectorAll(".progress-bar")!;
+    // progressBar.forEach(calculateProgressBar);
 
   },[])
 
@@ -201,27 +195,29 @@ const ItemSlider = ({
         <h3 className="m-0 text-xl">{title}</h3>
         <div className="progress-bar flex gap-2"></div>
       </div>
-      <div
-        className={`progress-container flex w-full items-center overflow-auto`}
+      <button
+        data-button={"previous"}
+        aria-label={"button for showing the previous items"}
+        className="button -translate-x-5 invisible rounded-l bg-opacity-20 bg-black text-[#f4f4f5] border border-[#f4f4f5] border-opacity-60 hover:bg-opacity-80 hover:bg-[#cccccf] z-10 h-fit m-0 cursor-pointer hover:text-[#09090a] active:text-[#cccccf] text-6xl items-center justify-center transition-all active:bg-[#cccccf]"
       >
-        <button
-          data-button={"previous"}
-          aria-label={"button for showing the previous items"}
-          className="button rounded-l bg-opacity-20 bg-black text-[#f4f4f5] border border-[#f4f4f5] border-opacity-60 hover:bg-opacity-80 hover:bg-[#cccccf] z-10 h-fit m-0 cursor-pointer hover:text-[#09090a] active:text-[#cccccf] text-6xl flex items-center justify-center transition-all active:bg-[#cccccf] -translate-x-12"
-        >
-          <span>&#8249;</span>
-        </button>
-        <nav data-title={title} className={`slider flex gap-4 m-0 w-full`}>
-          {children}
+        <span>&#8249;</span>
+      </button>
+      <div
+        className={`slider-container flex w-full items-center overflow-hidden`}
+      >
+        <nav data-title={title} className={`slider scroll-smooth w-full overflow-auto`}>
+          <div className={'children-container flex gap-4 m-0 w-full'} >
+            {children}
+          </div>
         </nav>
-        <button
-          aria-label={"button for showing the next items"}
-          data-button={"next"}
-          className="button absolute rounded-r bg-opacity-20 bg-black text-[#f4f4f5] border border-[#f4f4f5] border-opacity-60 hover:bg-opacity-80 hover:bg-[#cccccf] z-10 m-0 h-fit cursor-pointer hover:text-[#09090a] active:text-[#cccccf] text-6xl flex items-center justify-center transition-all active:bg-[#cccccf] translate-x-12"
-        >
-          <span>&#8250;</span>
-        </button>
       </div>
+      <button
+        aria-label={"button for showing the next items"}
+        data-button={"next"}
+        className="button translate-x-5 rounded-r bg-opacity-20 bg-black text-[#f4f4f5] border border-[#f4f4f5] border-opacity-60 hover:bg-opacity-80 hover:bg-[#cccccf] m-0 h-fit cursor-pointer hover:text-[#09090a] active:text-[#cccccf] text-6xl flex items-center justify-center transition-all active:bg-[#cccccf]"
+      >
+        <span>&#8250;</span>
+      </button>
     </section>
   );
 };
