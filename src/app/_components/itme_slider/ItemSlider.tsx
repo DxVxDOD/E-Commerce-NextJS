6f145dd2075652681bec82e61df4455e3d51aa58
DxVxDOD <orbangdavid@yahoo.com>
2023-10-30 12:16:13 +0200

    item sliders working as intended

--- src/app/_components/itme_slider/ItemSlider.tsx
+++ src/app/_components/itme_slider/ItemSlider.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { ReactNode, useEffect, useRef } from "react";
-import { generateActionId } from "next/dist/build/webpack/loaders/utils";
+import { ReactNode, useEffect } from "react";
 
 const ItemSlider = ({
   children,
@@ -10,7 +9,6 @@ const ItemSlider = ({
   children: ReactNode;
   title: string;
 }) => {
-
   const onButtonPressOld = (button: HTMLElement) => {
     const progressBar: HTMLElement = button
       .closest(".row")!
@@ -61,75 +59,103 @@ const ItemSlider = ({
   };
 
   // needs work, progress indicator doesn't move correctly
-  const moveProgressIndicator = (button: HTMLButtonElement, slider: HTMLElement) => {
-    const sliderIndex = parseInt(getComputedStyle(slider).getPropertyValue('--slider-index'));
-    const progressBar = button.closest('.row')!.querySelector('.progress-bar') as HTMLDivElement;
+  const moveProgressIndicator = (button: HTMLButtonElement) => {
+    const slider = button
+      .closest(".slider-container")!
+      .querySelector(".children-container") as HTMLElement;
+    const sliderIndex = parseInt(
+      getComputedStyle(slider).getPropertyValue("--slider-index"),
+    );
+    const progressBar = button
+      .closest(".row")!
+      .querySelector(".progress-bar") as HTMLDivElement;
     const progressBarItemCount = progressBar.children.length;
     const closestTitle: HTMLElement = button
       .closest(".row")!
       .querySelector(".slider")!;
 
-   if(button.dataset.button === 'next' && title === closestTitle.dataset.title) {
-     if (sliderIndex - 1 < 0) {
-       slider.style.setProperty(
-         "--slider-index",
-         `${progressBarItemCount - 1}`,
-       );
-       progressBar.children[sliderIndex].classList.remove("active");
-       progressBar.children[progressBarItemCount - 1].classList.add("active");
-     } else {
-       slider.style.setProperty("--slider-index", `${sliderIndex - 1}`);
-       progressBar.children[sliderIndex].classList.remove("active");
-       progressBar.children[sliderIndex - 1].classList.add("active");
-     }
-   }
-   if (button.dataset.button === 'previous' && title === closestTitle.dataset.title) {
-     if (sliderIndex + 1 >= progressBarItemCount) {
-       slider.style.setProperty("--slider-index", `${0}`);
-       progressBar.children[sliderIndex].classList.remove("active");
-       progressBar.children[0].classList.add("active");
-     } else {
-       slider.style.setProperty("--slider-index", `${sliderIndex + 1}`);
-       progressBar.children[sliderIndex].classList.remove("active");
-       progressBar.children[sliderIndex + 1].classList.add("active");
-     }
-   }
-  }
+    console.log(sliderIndex);
+
+    if (
+      button.dataset.button === "next" &&
+      title === closestTitle.dataset.title
+    ) {
+      if (sliderIndex - 1 < 0) {
+        slider.style.setProperty(
+          "--slider-index",
+          `${progressBarItemCount - 1}`,
+        );
+        progressBar.children[sliderIndex].classList.remove("active");
+        progressBar.children[progressBarItemCount - 1].classList.add("active");
+      } else {
+        slider.style.setProperty("--slider-index", `${sliderIndex - 1}`);
+        progressBar.children[sliderIndex].classList.remove("active");
+        progressBar.children[sliderIndex - 1].classList.add("active");
+      }
+    }
+    if (
+      button.dataset.button === "previous" &&
+      title === closestTitle.dataset.title
+    ) {
+      if (sliderIndex + 1 >= progressBarItemCount) {
+        slider.style.setProperty("--slider-index", `${0}`);
+        progressBar.children[sliderIndex].classList.remove("active");
+        progressBar.children[0].classList.add("active");
+      } else {
+        slider.style.setProperty("--slider-index", `${sliderIndex + 1}`);
+        progressBar.children[sliderIndex].classList.remove("active");
+        progressBar.children[sliderIndex + 1].classList.add("active");
+      }
+    }
+  };
 
   const onButtonPress = (container: HTMLElement) => {
-    const slider = container.querySelector('.slider') as HTMLElement;
-    const childrenContainer = slider.querySelector('.children-container') as HTMLElement;
-    const next = container.closest('.row')!.querySelector('[data-button="next"]') as HTMLButtonElement;
-    const previous = container.closest('.row')!.querySelector('[data-button="previous"]') as HTMLButtonElement;
+    const slider = container.querySelector(".slider") as HTMLElement;
+    const childrenContainer = slider.querySelector(
+      ".children-container",
+    ) as HTMLElement;
+    const next = container
+      .closest(".row")!
+      .querySelector('[data-button="next"]') as HTMLButtonElement;
+    const previous = container
+      .closest(".row")!
+      .querySelector('[data-button="previous"]') as HTMLButtonElement;
 
-    const gap = parseInt(getComputedStyle(childrenContainer).getPropertyValue('gap'));
+    const gap = parseInt(
+      getComputedStyle(childrenContainer).getPropertyValue("gap"),
+    );
     let width = container.offsetWidth;
 
-    next.addEventListener('click', e => {
+    next.addEventListener("click", (e) => {
       slider.scrollBy(width + gap, 0);
       if (slider.scrollWidth !== 0) {
-        previous.style.visibility = 'visible';
+        previous.style.visibility = "visible";
       }
-      if (childrenContainer.scrollWidth - width - gap <= slider.scrollLeft + width) {
-        next.style.visibility = 'hidden';
+      if (
+        childrenContainer.scrollWidth - width - gap <=
+        slider.scrollLeft + width
+      ) {
+        next.style.visibility = "hidden";
       }
-      // moveProgressIndicator(next, slider);
-    })
+      // moveProgressIndicator(next);
+    });
 
-    previous.addEventListener('click', e => {
+    previous.addEventListener("click", (e) => {
       slider.scrollBy(-(width + gap), 0);
       if (slider.scrollLeft - width - gap <= 0) {
-        previous.style.visibility = 'hidden';
+        previous.style.visibility = "hidden";
       }
-      if (childrenContainer.scrollWidth - width - gap <= slider.scrollLeft + width) {
-        next.style.visibility = 'visible'
+      if (
+        childrenContainer.scrollWidth - width - gap <=
+        slider.scrollLeft + width
+      ) {
+        next.style.visibility = "visible";
       }
-      // moveProgressIndicator(previous, slider);
-    })
+      // moveProgressIndicator(previous);
+    });
 
-
-    window.addEventListener('resize', e => (width = container.offsetWidth));
-  }
+    window.addEventListener("resize", (e) => (width = container.offsetWidth));
+  };
 
   const throttle = (callBackFunc: () => void, delay = 1000) => {
     const timeoutFunction = () => {
@@ -146,7 +172,7 @@ const ItemSlider = ({
     progressBar.innerHTML = "";
     const slider: HTMLElement = progressBar
       .closest(".row")!
-      .querySelector(".slider")!;
+      .querySelector(".children-container")!;
     const itemCount = slider.children.length;
     const itemPerScreen = parseInt(
       getComputedStyle(slider).getPropertyValue("--items-per-screen"),
@@ -172,8 +198,8 @@ const ItemSlider = ({
   };
 
   useEffect(() => {
-
-    const progressContainers: NodeListOf<HTMLElement> = document.querySelectorAll('.slider-container');
+    const progressContainers: NodeListOf<HTMLElement> =
+      document.querySelectorAll(".slider-container");
     progressContainers.forEach(onButtonPress);
 
     // window.addEventListener(
@@ -183,41 +209,43 @@ const ItemSlider = ({
     //     progressBar.forEach(calculateProgressBar);
     //   }, 250),
     // );
-    //
+
     // const progressBar = document.querySelectorAll(".progress-bar")!;
     // progressBar.forEach(calculateProgressBar);
-
-  },[])
+  }, []);
 
   return (
-    <section className="row border border-red-500 flex flex-col w-3/4 items-center p-4">
+    <section className="row flex flex-col w-3/4 border border-red-500 items-center p-4">
       <div className="flex justify-between w-full items-center p-2">
         <h3 className="m-0 text-xl">{title}</h3>
         <div className="progress-bar flex gap-2"></div>
       </div>
-      <button
-        data-button={"previous"}
-        aria-label={"button for showing the previous items"}
-        className="button -translate-x-5 invisible rounded-l bg-opacity-20 bg-black text-[#f4f4f5] border border-[#f4f4f5] border-opacity-60 hover:bg-opacity-80 hover:bg-[#cccccf] z-10 h-fit m-0 cursor-pointer hover:text-[#09090a] active:text-[#cccccf] text-6xl items-center justify-center transition-all active:bg-[#cccccf]"
-      >
-        <span>&#8249;</span>
-      </button>
       <div
         className={`slider-container flex w-full items-center overflow-hidden`}
       >
-        <nav data-title={title} className={`slider scroll-smooth w-full overflow-auto`}>
-          <div className={'children-container flex gap-4 m-0 w-full'} >
+        <button
+          data-button={"previous"}
+          aria-label={"button for showing the previous items"}
+          className="button invisible rounded-l -translate-x-12 bg-opacity-20 bg-black text-[#f4f4f5] border border-[#f4f4f5] border-opacity-60 hover:bg-opacity-80 hover:bg-[#cccccf] z-10 h-fit m-0 cursor-pointer hover:text-[#09090a] active:text-[#cccccf] text-6xl items-center justify-center transition-all active:bg-[#cccccf]"
+        >
+          <span>&#8249;</span>
+        </button>
+        <nav
+          data-title={title}
+          className={`slider scroll-smooth w-full overflow-auto`}
+        >
+          <div className={"children-container flex gap-4 m-0 w-full"}>
             {children}
           </div>
         </nav>
+        <button
+          aria-label={"button for showing the next items"}
+          data-button={"next"}
+          className="button rounded-r translate-x-12 bg-opacity-20 bg-black text-[#f4f4f5] border border-[#f4f4f5] border-opacity-60 hover:bg-opacity-80 hover:bg-[#cccccf] m-0 h-fit cursor-pointer hover:text-[#09090a] active:text-[#cccccf] text-6xl flex items-center justify-center transition-all active:bg-[#cccccf]"
+        >
+          <span>&#8250;</span>
+        </button>
       </div>
-      <button
-        aria-label={"button for showing the next items"}
-        data-button={"next"}
-        className="button translate-x-5 rounded-r bg-opacity-20 bg-black text-[#f4f4f5] border border-[#f4f4f5] border-opacity-60 hover:bg-opacity-80 hover:bg-[#cccccf] m-0 h-fit cursor-pointer hover:text-[#09090a] active:text-[#cccccf] text-6xl flex items-center justify-center transition-all active:bg-[#cccccf]"
-      >
-        <span>&#8250;</span>
-      </button>
     </section>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,21 @@
 import AdSlider from "@/app/_components/ad_slider/AdSlider";
 import ItemSlider from "@/app/_components/itme_slider/ItemSlider";
-import computer_accessories from '@/public/computer-accessories.svg';
-import computer_components from '@/public/computer_components.svg';
-import computers from '@/public/computers.svg';
-import screens from '@/public/screens.svg';
-import smart_home from '@/public/smart_home.svg';
-import game_console_vr from '@/public/game_console_vr.svg';
-import external_storage from '@/public/external_storage.svg';
-import home_leisure_health from '@/public/home_leisure_health.svg';
-import cable_adapters from '@/public/cable_adapters.svg';
-import sound_image from '@/public/sound_image.svg';
-import mobile_phones from '@/public/mobile_phone.svg';
-import network from '@/public/network.svg';
-import gift_cards from '@/public/gift_cards.svg';
-import software from '@/public/software.svg'
-import printer_scanner from '@/public/printer_scanner.svg';
-import tablet from '@/public/tablet.svg'
+import computer_accessories from "@/public/computer-accessories.svg";
+import computer_components from "@/public/computer_components.svg";
+import computers from "@/public/computers.svg";
+import screens from "@/public/screens.svg";
+import smart_home from "@/public/smart_home.svg";
+import game_console_vr from "@/public/game_console_vr.svg";
+import external_storage from "@/public/external_storage.svg";
+import home_leisure_health from "@/public/home_leisure_health.svg";
+import cable_adapters from "@/public/cable_adapters.svg";
+import sound_image from "@/public/sound_image.svg";
+import mobile_phones from "@/public/mobile_phone.svg";
+import network from "@/public/network.svg";
+import gift_cards from "@/public/gift_cards.svg";
+import software from "@/public/software.svg";
+import printer_scanner from "@/public/printer_scanner.svg";
+import tablet from "@/public/tablet.svg";
 import Image from "next/image";
 import Card from "@/app/_components/Card";
 
@@ -23,7 +23,7 @@ export default function Home() {
   return (
     <main className=" flex flex-col items-center gap-4 w-full h-full">
       <AdSlider />
-      <ItemSlider title={'Test'} >
+      <ItemSlider title={"Test"}>
         <img src="https://via.placeholder.com/210/00FF00?text=1" />
         <img src="https://via.placeholder.com/220/00FF00?text=2" />
         <img src="https://via.placeholder.com/230/00FF00?text=3" />
@@ -37,86 +37,86 @@ export default function Home() {
         <img src="https://via.placeholder.com/270/00FF00?text=11" />
         <img src="https://via.placeholder.com/280/00FF00?text=12" />
       </ItemSlider>
-      <ItemSlider title={'Categories'}>
-        <Card categories={true} link={''}>
-          <Image src={computers} alt={'Computers'}/>
+      <ItemSlider title={"Categories"}>
+        <Card categories={true} link={""}>
+          <Image src={computers} alt={"Computers"} />
         </Card>
-        <Card categories={true} link={''}>
-          <Image src={computer_components} alt={'Computer Components'}/>
+        <Card categories={true} link={""}>
+          <Image src={computer_components} alt={"Computer Components"} />
         </Card>
-        <Card categories={true} link={''}>
-          <Image src={screens} alt={'Screens'}/>
+        <Card categories={true} link={""}>
+          <Image src={screens} alt={"Screens"} />
         </Card>
-        <Card categories={true} link={''}>
-          <Image src={computer_accessories} alt={'Computer Accessories'}/>
+        <Card categories={true} link={""}>
+          <Image src={computer_accessories} alt={"Computer Accessories"} />
         </Card>
-        <Card categories={true} link={''}>
-          <Image src={smart_home} alt={'Smart Home'}/>
+        <Card categories={true} link={""}>
+          <Image src={smart_home} alt={"Smart Home"} />
         </Card>
-        <Card categories={true} link={''}>
-        <Image src={game_console_vr} alt={'Game | Console | VR'}/>
+        <Card categories={true} link={""}>
+          <Image src={game_console_vr} alt={"Game | Console | VR"} />
         </Card>
-        <Card categories={true} link={''}>
-          <Image src={external_storage} alt={'External Storage'}/>
+        <Card categories={true} link={""}>
+          <Image src={external_storage} alt={"External Storage"} />
         </Card>
-        <Card categories={true} link={''}>
-          <Image src={home_leisure_health} alt={'Home | Leisure | Health'}/>
+        <Card categories={true} link={""}>
+          <Image src={home_leisure_health} alt={"Home | Leisure | Health"} />
         </Card>
-        <Card categories={true} link={''}>
-          <Image src={cable_adapters} alt={'Cable | Adapters'}/>
+        <Card categories={true} link={""}>
+          <Image src={cable_adapters} alt={"Cable | Adapters"} />
         </Card>
-        <Card categories={true} link={''}>
-          <Image src={sound_image} alt={'Sound | Image'}/>
+        <Card categories={true} link={""}>
+          <Image src={sound_image} alt={"Sound | Image"} />
         </Card>
-        <Card categories={true} link={''}>
-          <Image src={mobile_phones} alt={'Mobile Phones'}/>
+        <Card categories={true} link={""}>
+          <Image src={mobile_phones} alt={"Mobile Phones"} />
         </Card>
-        <Card categories={true} link={''}>
-          <Image src={network} alt={'Network'}/>
+        <Card categories={true} link={""}>
+          <Image src={network} alt={"Network"} />
         </Card>
-        <Card categories={true} link={''}>
-          <Image src={gift_cards} alt={'Gift Cards'}/>
+        <Card categories={true} link={""}>
+          <Image src={gift_cards} alt={"Gift Cards"} />
         </Card>
-        <Card categories={true} link={''}>
-          <Image src={software} alt={'Software'}/>
+        <Card categories={true} link={""}>
+          <Image src={software} alt={"Software"} />
         </Card>
-        <Card categories={true} link={''}>
-          <Image src={printer_scanner} alt={'Printer | Scanner'}/>
+        <Card categories={true} link={""}>
+          <Image src={printer_scanner} alt={"Printer | Scanner"} />
         </Card>
-        <Card categories={true} link={''}>
-          <Image src={tablet} alt={'Tablets'}/>
+        <Card categories={true} link={""}>
+          <Image src={tablet} alt={"Tablets"} />
         </Card>
       </ItemSlider>
-      <ItemSlider title={'Promotions'}>
-        <Card link={''} categories={false}>
-          <img alt={''} src="https://via.placeholder.com/210/00FF00?text=1" />
+      <ItemSlider title={"Promotions"}>
+        <Card link={""} categories={false}>
+          <img alt={""} src="https://via.placeholder.com/210/00FF00?text=1" />
         </Card>
-        <Card link={''} categories={false}>
-          <img alt={''} src="https://via.placeholder.com/210/00FF00?text=1" />
+        <Card link={""} categories={false}>
+          <img alt={""} src="https://via.placeholder.com/210/00FF00?text=1" />
         </Card>
-        <Card link={''} categories={false}>
-          <img alt={''} src="https://via.placeholder.com/210/00FF00?text=1" />
+        <Card link={""} categories={false}>
+          <img alt={""} src="https://via.placeholder.com/210/00FF00?text=1" />
         </Card>
-        <Card link={''} categories={false}>
-          <img alt={''} src="https://via.placeholder.com/210/00FF00?text=1" />
+        <Card link={""} categories={false}>
+          <img alt={""} src="https://via.placeholder.com/210/00FF00?text=1" />
         </Card>
-        <Card link={''} categories={false}>
-          <img alt={''} src="https://via.placeholder.com/210/00FF00?text=1" />
+        <Card link={""} categories={false}>
+          <img alt={""} src="https://via.placeholder.com/210/00FF00?text=1" />
         </Card>
-        <Card link={''} categories={false}>
-          <img alt={''} src="https://via.placeholder.com/210/00FF00?text=1" />
+        <Card link={""} categories={false}>
+          <img alt={""} src="https://via.placeholder.com/210/00FF00?text=1" />
         </Card>
-        <Card link={''} categories={false}>
-          <img alt={''} src="https://via.placeholder.com/210/00FF00?text=1" />
+        <Card link={""} categories={false}>
+          <img alt={""} src="https://via.placeholder.com/210/00FF00?text=1" />
         </Card>
-        <Card link={''} categories={false}>
-          <img alt={''} src="https://via.placeholder.com/210/00FF00?text=1" />
+        <Card link={""} categories={false}>
+          <img alt={""} src="https://via.placeholder.com/210/00FF00?text=1" />
         </Card>
-        <Card link={''} categories={false}>
-          <img alt={''} src="https://via.placeholder.com/210/00FF00?text=1" />
+        <Card link={""} categories={false}>
+          <img alt={""} src="https://via.placeholder.com/210/00FF00?text=1" />
         </Card>
-        <Card link={''} categories={false}>
-          <img alt={''} src="https://via.placeholder.com/210/00FF00?text=1" />
+        <Card link={""} categories={false}>
+          <img alt={""} src="https://via.placeholder.com/210/00FF00?text=1" />
         </Card>
       </ItemSlider>
     </main>
